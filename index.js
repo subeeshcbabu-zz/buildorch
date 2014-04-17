579a021b0a620e217688d26daaa72d8ba6e2124e
suchothendav <suchothendav@paypal.com>
2014-04-16 23:43:30 -0700

    grunt cli integration

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-'use strict';
+"use strict";
 //Now supporting CLI only, module based access in Progress.
 
---
+++ test/fixtures/Gruntfile.js
@@ -0,0 +1,26 @@
+var grunt = require('grunt');
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  	pkg: grunt.file.readJSON('package.json'),
+  	jshint: {
+  		files: ['index.js'],
+  		options: {
+  			jshintrc: '.jshintrc'
+  		}
+  	}
+  });
+
+  // Load the plugin that provides the "jshint" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task(s).
+  grunt.registerTask('lint', ['jshint']);
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('coverage', ['jshint']);
+
+  grunt.registerTask('default', ['test']);
+
+};
